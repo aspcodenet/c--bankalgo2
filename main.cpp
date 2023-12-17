47b402d462fbf6ac32f1f4a3cc738cf2fa24305c
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-12-17 15:04:55 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+
+class BankAccount{
+    std::string accountNumber;
+    float balance;
+public:
+    BankAccount(std::string accountNumber, float balance = 0)
+    :accountNumber(accountNumber),balance(balance)
+    {
+    }
+
+    std::string getAccountNumber()
+    {
+	    return this->accountNumber;
+    }
+};
+
+class IAccountStorage {
+public:    
+    virtual void addAccount(BankAccount account) = 0;
+	virtual BankAccount *findAccount(std::string accountNumber) = 0;        
+};
+
+
+
+class Bank
+{
+private:
+	IAccountStorage * accountStorage;
+public:
+	Bank(IAccountStorage *storage):accountStorage(storage){
+
+    }
+	bool addAccount(std::string accountNumber){
+        //validate
+        //if something (accountNumber) return false
+        accountStorage->addAccount(accountNumber);
+        return true;
+    }
+	BankAccount *getAccount(std::string accountNumber){
+        return accountStorage->findAccount(accountNumber);
+    }
+};
+
+class VectorAccountStorage: public IAccountStorage{
+        std::vector<BankAccount> accounts;
+public:
+    void addAccount(BankAccount account) override{
+        accounts.push_back(account);
+    }
+
+    BankAccount *findAccount(std::string accountNumber){
+        BankAccount *ret = NULL;
+        for(BankAccount &account : accounts){
+            if(account.getAccountNumber() == accountNumber ){
+                ret = &account;                                        
+            }
+        }
+        return ret;
+    }
+    
+
+};
+
+
+
+int main(int, char**){
+    VectorAccountStorage storage;
+    Bank bank(&storage);
+
+    const int AntalAccounts =  1000000;
+
+
+    std::string sFirst = ""; 
+    std::string sLast = ""; 
+    std::string sNotFound = "notfound"; 
+    std::cout << "INITIALIZE: " << std::endl;
+    auto startTime = std::chrono::high_resolution_clock::now();
+    for(int i = 0;i < AntalAccounts; i++){
+        std::string accountNumber =  std::to_string(i);
+        if(i == 0){
+            sFirst = accountNumber;
+        }
+        if(i == AntalAccounts-1){
+            sLast = accountNumber;
+        }
+        bank.addAccount(std::to_string(i));
+    }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    std::cout << "INIT Took: " << std::chrono::duration_cast<std::chrono::milliseconds>(endTime    - startTime).count() << " milliseconds" << std::endl;
+
+    startTime = std::chrono::high_resolution_clock::now();
+    BankAccount *p = bank.getAccount(sFirst);
+    endTime = std::chrono::high_resolution_clock::now();
+    std::cout << p->getAccountNumber() << " took: " << std::chrono::duration_cast<std::chrono::milliseconds>(endTime    - startTime).count() << " milliseconds" << std::endl;
+
+    startTime = std::chrono::high_resolution_clock::now();
+    p = bank.getAccount(sLast);
+    endTime = std::chrono::high_resolution_clock::now();
+    std::cout << p->getAccountNumber() << " took: " << std::chrono::duration_cast<std::chrono::milliseconds>(endTime    - startTime).count() << " milliseconds" << std::endl;
+
+    startTime = std::chrono::high_resolution_clock::now();
+    p = bank.getAccount(sNotFound);
+    endTime = std::chrono::high_resolution_clock::now();
+    std::cout << "NOT FOUND" << " took: " << std::chrono::duration_cast<std::chrono::milliseconds>(endTime    - startTime).count() << " milliseconds" << std::endl;
+}
